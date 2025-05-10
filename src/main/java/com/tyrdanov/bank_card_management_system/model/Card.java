f3bce9ba01d0fbeaaa129ec9f450babbab396c79
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-05-11 00:45:27 +0300

    #9 feat: add unit tests for services

--- src/main/java/com/tyrdanov/bank_card_management_system/model/Card.java
+++ src/main/java/com/tyrdanov/bank_card_management_system/model/Card.java
@@ -14,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import lombok.experimental.FieldDefaults;
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
